36c93bf918cfa993c2575ccafa1c685e89478dcf
Micah Robinson <micahrobinsondesigns@gmail.com>
2023-02-02 20:59:56 -0500

    Updated URLs

--- precache-manifest.6952b845515052b57521896898be8e30.js
+++ precache-manifest.6952b845515052b57521896898be8e30.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/how-long-for-alcohol/static/media/logo.5d5d9eef.svg"
+    "url": "/static/media/logo.5d5d9eef.svg"
   },
   {
     "revision": "ff1258cdf92fce207c2f",
-    "url": "/how-long-for-alcohol/static/js/runtime~main.ff1258cd.js"
+    "url": "/static/js/runtime~main.ff1258cd.js"
   },
   {
     "revision": "682a02dfc872425e3ce3",
-    "url": "/how-long-for-alcohol/static/js/main.682a02df.chunk.js"
+    "url": "/static/js/main.682a02df.chunk.js"
   },
   {
     "revision": "f739feb7d61c48a46bed",
-    "url": "/how-long-for-alcohol/static/js/1.f739feb7.chunk.js"
+    "url": "/static/js/1.f739feb7.chunk.js"
   },
   {
     "revision": "682a02dfc872425e3ce3",
-    "url": "/how-long-for-alcohol/static/css/main.982508c8.chunk.css"
+    "url": "/static/css/main.982508c8.chunk.css"
   },
   {
     "revision": "d7fbd8c46545579aaf7ea0c6a761ff5b",
-    "url": "/how-long-for-alcohol/index.html"
+    "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/how-long-for-alcohol/precache-manifest.6952b845515052b57521896898be8e30.js"
+  "/precache-manifest.6952b845515052b57521896898be8e30.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/how-long-for-alcohol/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
